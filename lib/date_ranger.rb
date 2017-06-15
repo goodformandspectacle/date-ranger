358260b94d4ceed39367e7c8efc12c335a902c48
ntlk <hi@ntlk.net>
2017-06-15 11:02:23 +0100

    Initial commit

---
+++ lib/date_ranger.rb
@@ -0,0 +1,89 @@
+require 'date'
+
+DateRanger = Struct.new(:date_str) do
+  def parse
+    # basic cleanup
+    date_str.gsub!('c.', '')
+    date_str.gsub!('circa', '')
+    date_str.strip!
+
+    if date_str.include? 'n.d.'
+      # no date marker
+      ranger_hash(nil, nil)
+    elsif (date_str.end_with? 's')
+      # we know that we’re dealing with a decade
+      decade_str = date_str.chomp('s')
+      ranger_hash(beginning_of_decade(decade_str), end_of_decade(decade_str))
+    elsif (is_year?(date_str))
+      # we have just a 4-digit year
+      ranger_hash(beginning_of_year(date_str), end_of_year(date_str))
+    elsif (is_month_and_year?(date_str))
+      # we have a 'Nov 1756' format
+      ranger_hash(beginning_of_month(date_str), end_of_month(date_str))
+    elsif (is_day_month_and_year?(date_str))
+      # we have a '23 Feb 1985' format
+      ranger_hash(beginning_of_day(date_str), end_of_day(date_str))
+    end
+  end
+
+  def ranger_hash beginning_date, end_date
+    { beginning_date: beginning_date, end_date: end_date }
+  end
+
+  def beginning_of_day day_str
+    date = Date._parse(day_str)
+    Date.new(date[:year], date[:mon], date[:mday])
+  end
+
+  def end_of_day day_str
+    beginning_of_day day_str
+  end
+
+  def beginning_of_month month_str
+    date = Date._parse(month_str)
+    Date.new(date[:year], date[:mon], 1)
+  end
+
+  def end_of_month month_str
+    date = Date._parse(month_str)
+    Date.new(date[:year], date[:mon], -1)
+  end
+
+  def beginning_of_year year_str
+    Date.new(year_str.to_i, 1, 1)
+  end
+
+  def end_of_year year_str
+    Date.new(year_str.to_i, 12, 31)
+  end
+
+  def beginning_of_decade decade_str
+    Date.new(decade_str.to_i, 1, 1)
+  end
+
+  def end_of_decade decade_str
+    Date.new(decade_str.to_i + 9, 12, 31)
+  end
+
+  def is_year?(date_str, start_time=1500, end_time=2099)
+    # check that we have only the year
+    (start_time..end_time).include?(date_str.to_i)
+  end
+
+  def is_month_and_year? date_str
+    # check that we have only month and year
+    date_hash = Date._parse(date_str)
+    date_hash.has_key?(:year) and date_hash.has_key?(:mon) and !date_hash.has_key?(:mday)
+  end
+
+  def is_day_month_and_year? date_str
+    Date.strptime(date_str, '%d %b %Y') rescue false
+  end
+
+  def cleanup str
+    str.gsub!('c.', '')
+    str.gsub!('circa', '')
+    str.strip!
+  end
+
+end
---
+++ spec/date_ranger_spec.rb
@@ -0,0 +1,44 @@
+require 'date_ranger'
+
+RSpec.describe 'DateRanger' do
+  def result_hash beginning_date, end_date
+    { beginning_date: beginning_date, end_date: end_date }
+  end
+
+  it 'converts 1990s to a range covering the nineties' do
+    date_str = '1990s'
+    beginning_of_nineties = Date.new(1990, 1, 1)
+    end_of_nineties = Date.new(1999, 12, 31)
+    expect(DateRanger.new(date_str).parse).to eq result_hash(beginning_of_nineties, end_of_nineties)
+  end
+
+  it 'converts n.d. to no date' do
+    date_str = 'n.d.'
+    expect(DateRanger.new(date_str).parse).to eq result_hash(nil, nil)
+  end
+
+  it 'converts a year to a year range' do
+    date_str = '1975'
+    expect(DateRanger.new(date_str).parse).to eq result_hash(Date.new(1975, 1, 1), Date.new(1975, 12, 31))
+  end
+
+  it 'converts c. 1856 type date to a year range' do
+    date_str = 'c. 1945'
+    expect(DateRanger.new(date_str).parse).to eq result_hash(Date.new(1945, 1, 1), Date.new(1945, 12, 31))
+  end
+
+  it 'converts a month to a month range' do
+    date_str = 'Nov 1867'
+    expect(DateRanger.new(date_str).parse).to eq result_hash(Date.new(1867, 11, 1), Date.new(1867, 11, 30))
+  end
+
+  it 'converts c. Nov 1856 type date to a month range' do
+    date_str = 'c. Nov 1945'
+    expect(DateRanger.new(date_str).parse).to eq result_hash(Date.new(1945, 11, 1), Date.new(1945, 11, 30))
+  end
+
+  it 'converts 17 Feb 1865 date to a day range' do
+    date_str = '17 Feb 1945'
+    expect(DateRanger.new(date_str).parse).to eq result_hash(Date.new(1945, 2, 17), Date.new(1945, 2, 17))
+  end
+end
